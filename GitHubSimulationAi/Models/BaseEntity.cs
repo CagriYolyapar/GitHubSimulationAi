d13096927de665e8a71e0ebfefe167772018e81c
CagriYolyapar <cagri.yolyapar@gmail.com>
2025-03-04 03:40:15 +0300

    Classlar eklendi

---
+++ GitHubSimulationAi/Models/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubSimulationAi.Models
+{
+    public abstract class BaseEntity
+    {
+        public string ShipName { get; set; }
+        public string ShipShield { get; set; }
+        public int ShipHealth { get; set; }
+        public int ShipArmor { get; set; }
+
+    }
+}
